c33eb433428d285502da478fc13910663713fe68
edoardottt <edoardott@gmail.com>
2022-11-04 17:40:12 +0100

    update moderations

--- pkg/services/moderations.go
+++ pkg/services/moderations.go
@@ -1,1 +1,19 @@
 package services
+
+import (
+	"context"
+
+	"github.com/edoardottt/dalle/pkg/api"
+)
+
+// CreateModeration classifies if text violates OpenAI's Content Policy.
+func (a *API) CreateModeration(ctx context.Context, input *api.ModerationInput) (api.Moderations, error) {
+	var response api.Moderations
+
+	path := `/moderations`
+	if err := a.client.Post(ctx, path, &response, input); err != nil {
+		return api.Moderations{}, err
+	}
+
+	return response, nil
+}
