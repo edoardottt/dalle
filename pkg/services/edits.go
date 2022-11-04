c51e775a00f73df332a6228ecc686793c760e6cb
edoardottt <edoardott@gmail.com>
2022-11-04 17:12:11 +0100

    update completions + edits

--- pkg/services/edits.go
+++ pkg/services/edits.go
@@ -1,1 +1,19 @@
 package services
+
+import (
+	"context"
+
+	"github.com/edoardottt/dalle/pkg/api"
+)
+
+// CreateEdit creates a new edit for the provided input, instruction, and parameters.
+func (a *API) CreateEdit(ctx context.Context, input *api.EditInput) (api.Edit, error) {
+	var response api.Edit
+
+	path := `/edits`
+	if err := a.client.Post(ctx, path, &response, input); err != nil {
+		return api.Edit{}, err
+	}
+
+	return response, nil
+}
